904ad882dfb55c7e391c83510a1f5e5a9066f129
“get-bit” <maya.chump_0n@icloud.com>
2025-02-12 12:18:35 -0500

    initial commit with authentication functions

---
+++ router/auth_users.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+const isValid = (username)=>{ //returns boolean
+// Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+// Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const authenticatedUser = (username,password)=>{ //returns boolean
+// Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+// Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+}}
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!doesExist(username)) {
+        // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
